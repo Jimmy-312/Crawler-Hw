2d3cc9094709252689a7915f141f70701961f0d4
Jimmy-312 <2489219080@qq.com>
2022-04-01 18:12:08 +0800

    nexushd completed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from nexushd import nexushd_scraper
-name = "Star Wars"
 
 with open("user.txt","rb") as f:
-    content = f.read()
+    content = f.read().decode("utf-8")
     content = content.split(",")
     username,password = content[0],content[1]
+
+name = "Star Wars"
 
 nexushd = nexushd_scraper()
 nexushd.login(username,password)
